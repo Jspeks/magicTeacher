d6bf815f2025366fee819edb8c1a2afe6c5e9c75
unknown <jessespeks@hotmail.com>
2022-07-25 19:49:16 -0400

    solved the untap problem, working on showing mana produced

--- theWorks.js
+++ theWorks.js
@@ -1,21 +1,75 @@
+
+// layout some variables
+const hand = document.getElementById("hand");
+const permanents = document.getElementById("permanents");
+const landsInPlay = document.getElementById("lands");
+const library = document.getElementById("library");
+const lifeDiv = document.getElementById("life");
+const manaPool = document.getElementById("manaPool");
+
+const whiteMana = document.querySelectorAll(".whiteMana");
+const greenMana = document.querySelectorAll(".greenMana");
+const catCat = document.querySelectorAll(".catCat");
+const romeo = document.querySelectorAll(".romeo");
+const nerd = document.querySelectorAll(".nerdAlert");
+const tutu = document.querySelectorAll(".tutu");
+const plains = document.querySelectorAll(".plains");
+const forest = document.querySelectorAll(".forest");
+const creature = document.querySelectorAll(".creature");
+const manaProducingLand = document.querySelectorAll(".lands")
+
+let lifeTotal = 20;
+let captainHindsight = document.getElementById("moveTaken");
+
 // need to make some dummies for what I'll need the game to do
 function Intro(){};
-function main(){};
+function main(){
+        lifeDiv.append(lifeTotal)
+};
 function changePhase(){};
+function untapStep() {
+    console.log(creature)
+    creature.forEach((c) => c.classList.remove("tapped"))
+    creature.forEach((c) => c.style.transform ="rotate(0deg)")
+}
+
+// ability to produce mana here
+const whiteImg = {
+    src: "./assets/whiteSymbol.png",
+    classes: ["mana", "whiteMana"]
+}
+const greenIMG = {
+    src: "./assets/greenSymbol.png",
+    classes: ["mana", "greenMana"]
+}
+const manaImg = {whiteImg, greenIMG}
+
 function tapForMana(e){
     e.target.style.transform = "rotate(90deg)";
     captainHindsight.textContent = "You attempted to produce mana, but my code isn't done yet";
     setTimeout(silence, 4000);
+    let manaSymbol = document.createElement('img')
+    manaSymbol.src = manaImg[0].src;
+    for (let i=0; i<manaImg[0].classes.length; i++){
+        manaSymbol.classList.add(manaImg[0].classes[i]);
+    }
+    manaPool.append(manaSymbol);
 };
+
+
 function tapToAttack(e) {
     e.target.style.transform = "rotate(90deg)";
     captainHindsight.textContent = "You attempted to attack, but my code isn't done yet";
     setTimeout(silence, 4000);
+    e.target.classList.add("tapped")
+    console.log(permanents)
 };
 function silence(){
     captainHindsight.textContent = ""
 };
-function dealDamage(){};
+function dealDamage(){
+
+};
 function playSpellFromHand(){};
 function playLandFromHand(){};
 
@@ -37,47 +91,27 @@ function victory(){};
 function defeat(){};
 function whatIs() {
     captainHindsight.textContent = "That is your opponent's life total. Bring that number down to zero by attacking with your creatures to win the game!";
-    setTimeout(silence, 5000);
+    setTimeout(silence, 5000); 
+    untapStep();   
 }
-
-// layout some variables
-const hand = document.getElementById("hand")
-const permanents = document.getElementById("permanents")
-const manaPool = document.getElementById("lands")
-const library = document.getElementById("library");
-const lifeDiv = document.getElementById("life")
-
-const whiteMana = document.querySelectorAll(".whiteMana");
-const greenMana = document.querySelectorAll(".greenMana");
-const catCat = document.querySelectorAll(".catCat");
-const romeo = document.querySelectorAll(".romeo");
-const nerd = document.querySelectorAll(".nerdAlert");
-const tutu = document.querySelectorAll(".tutu");
-const plains = document.querySelectorAll(".plains");
-const forest = document.querySelectorAll(".forest");
-const creature = document.querySelectorAll(".creature");
-const manaProducingLand = document.querySelectorAll(".lands")
-
-let lifeTotal = 20;
-let captainHindsight = document.getElementById("moveTaken");
 
 // make a deck of "cards"
 
 let c1 = {
     src: "./assets/romeo.png",
-    classes: ["card", "creature", "romeo"]
+    classes: ["card", "creature", "romeo", 1]
 }
 let c2 = {
     src: "./assets/twoTwo.png",
-    classes: ["card", "creature", "tutu"]
+    classes: ["card", "creature", "tutu", 2]
 }
 let c3 = {
     src: "./assets/catCat.png",
-    classes: ["card", "creature", "catCat"]
+    classes: ["card", "creature", "catCat", 1]
 }
 let c4 = {
     src: "./assets/nerdAlert.png",
-    classes: ["card", "creature", "nerdAlert"]
+    classes: ["card", "creature", "nerdAlert", 2]
 }
 let l1 = {
     src: "./assets/plains.png",
@@ -102,7 +136,9 @@ let deckOfCards = [c1, l1, c2, c1, l1, c3, l2, l1, c4, l1, c2, c4, c3, l2, c1, l
 // need to add event listener for attack in the play spell function 
 // etc.. 
 
+main()
 lifeDiv.addEventListener('mouseover', whatIs)
 creature.forEach(c => c.addEventListener('click', tapToAttack));
 manaProducingLand.forEach(m => m.addEventListener('click', tapForMana));
 library.addEventListener('click', drawACard)
+console.log(permanents)
